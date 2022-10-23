285adbbdf50b37aabc358fe9068bc1133221649f
rle253 <rle253@cs.washington.edu>
2022-10-23 02:56:12 -0700

    db works and menu is complete

--- public/index.js
+++ public/index.js
@@ -1,267 +1,284 @@
- 
 "use strict";
- const RANDOM_QUOTE_API_URL = "http://api.quotable.io/random";
 
- (function() {
-    const quoteDisplayElement = id('quote_display');
-    const timerElement = id('timer');
+const RANDOM_QUOTE_API_URL = "http://api.quotable.io/random";
 
-    let arrayQuote = [];
-    let curIndex = 0;
-    let valueWord = "";
-    let can;
-    // MODULE GLOBAL VARIABLES, CONSTANTS, AND HELPER FUNCTIONS CAN BE PLACED HERE
+(function() {
 
+   const quoteDisplayElement = id('quote_display');
+   const timerElement = id('timer');
 
+   let arrayQuote = [];
+   let curIndex = 0;
+   let valueWord = "";
 
-    // Import the functions you need from the SDKs you need
-
-    // TODO: Add SDKs for Firebase products that you want to use
-    // https://firebase.google.com/docs/web/setup#available-libraries
-  
-    // Your web app's Firebase configuration
-    // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-    const firebaseConfig = {
-      apiKey: "AIzaSyCsmqZ8Y87u9k3xofzcMapRdleSDQQGjPQ",
-      authDomain: "write-racer.firebaseapp.com",
-      databaseURL: "https://write-racer-default-rtdb.firebaseio.com/",
-      projectId: "write-racer",
-      storageBucket: "write-racer.appspot.com",
-      messagingSenderId: "630514342479",
-      appId: "1:630514342479:web:6ad281678526d0a731683a",
-      measurementId: "G-FGC783LTR9"
-    };
-
-    // Initialize Firebase
-    let app;
-    let analytics;
-    let database;
+   let can;
+   // MODULE GLOBAL VARIABLES, CONSTANTS, AND HELPER FUNCTIONS CAN BE PLACED HERE
 
 
-    /**
-    * Add a function that will be called when the window is loaded.
-    */
-    window.addEventListener("load", init);
+   let playerId;
+   let playerRef;
 
-    /**
-    * CHANGE: Describe what your init function does here.
-    */
-    function init() {
-      console.log(firebase);
+   firebase.auth().signInAnonymously().catch((error)=> {
+    let errorCode = error.code;
+    let errorMessage = error.message;
+    console.log(errorCode, errorMessage);
+   });
 
+   firebase.auth().onAuthStateChanged((user) => {
+    console.log(user);
+    if (user) {
+      console.log("you're logged in");
+      playerId = user.uid;
 
-      console.log("hi");
-      // THIS IS THE CODE THAT WILL BE EXECUTED ONCE THE WEBPAGE LOADS
-      renderNewQuote();
-      generateCanvas();
+      // console.log(playerId);
+      // let Games = {
+      //   "numWords": 10,
+      //   "timer": 0,
+      //   "userState": "temp",
+      //   "users": "me",
+      //   "winner": false,
+      //   "words": "temp"
+      // }
+
+      // Games = JSON.stringify(Games);
+      // playerRef = firebase.database().ref(`Games/${playerId}`);
+
+      // playerRef.set({
+      //   numWords: 10,
+      //   timer: 0,
+      //   userState: "hi",
+      //   users:"ryan",
+      //   winner:false,
+      //   words:"temp"
+      // });
+      playerRef.onDisconnect().remove();
+    } else {
+      console.log("you're NOT logged in");
     }
+  });
 
-    function checkInput() {
-      console.log("changed");
-      let correct = true;
-      const arrayQuoteSpan = quoteDisplayElement.querySelectorAll('span');
-      
-      if(valueWord == null) {
-        arrayQuoteSpan[curIndex].classList.remove('correct');
-        arrayQuoteSpan[curIndex].classList.remove('incorrect');
-        correct = false;
-      }
-      else if(arrayQuote[curIndex] === valueWord) {
-        arrayQuoteSpan[curIndex].classList.add('correct');
-        arrayQuoteSpan[curIndex].classList.remove('incorrect');
-        // correct word, moving onto next
-        curIndex++;
-        can.erase();
-        clearDisplayText();
+   /**
+   * Add a function that will be called when the window is loaded.
+   */
+   window.addEventListener("load", init);
 
-      } else {
-        arrayQuoteSpan[curIndex].classList.remove('correct');
-        arrayQuoteSpan[curIndex].classList.add('incorrect');
-        correct = false;
-      }
-
-      if(curIndex === arrayQuote.length && correct) renderNewQuote();
-    }
-
-    function renderNewQuote() {
-      curIndex = 0;
-      arrayQuote = [];
-      const quote = getRandomQuote();
-      quoteDisplayElement.innerHTML = '';
-      quote.split(" ").forEach(word => {
-        const wordSpan = gen('span');
-        wordSpan.innerText = word + " ";
-        quoteDisplayElement.appendChild(wordSpan);
-        arrayQuote.push(word);
-      })
-
-      startTimer();
-    }
-
-    let startTime;
-    function startTimer() {
-      timerElement.innerText = 5;
-      startTime = new Date();
-      let timerId = setInterval(() => {
-        timer.innerText = 5 - getTimerTime();
-        if(Number(timer.textContent) === 0) {
-          clearInterval(timerId);
-        }
-      }, 1000);
-    }
-
-    function getTimerTime() {
-      return Math.floor((new Date() - startTime) / 1000);
-    }
-
-    /**
-    * Make sure to always add a descriptive comment above
-    * every function detailing what it's purpose is
-    * @param {variabletype} someVariable This is a description of someVariable, including, perhaps, preconditions.
-    * @returns {returntype} A description of what this function is actually returning
-    */
-    function exampleFunction2(someVariable) {
-      /* SOME CODE */
-      return something;
-    }
-
-    /** ------------------------------ Helper Functions  ------------------------------ */
-    /**
-    * Note: You may use these in your code, but remember that your code should not have
-    * unused functions. Remove this comment in your own code.
-    */
-
-    /**
-    * Returns the element that has the ID attribute with the specified value.
-    * @param {string} idName - element ID
-    * @returns {object} DOM object associated with id.
-    */
-    function id(idName) {
-      return document.getElementById(idName);
-    }
-
-    /**
-    * Returns the first element that matches the given CSS selector.
-    * @param {string} selector - CSS query selector.
-    * @returns {object} The first DOM object matching the query.
-    */
-    function qs(selector) {
-      return document.querySelector(selector);
-    }
-
-    /**
-    * Returns the array of elements that match the given CSS selector.
-    * @param {string} selector - CSS query selector
-    * @returns {object[]} array of DOM objects matching the query.
-    */
-    function qsa(selector) {
-      return document.querySelectorAll(selector);
-    }
-
-    /**
-    * Returns a new element with the given tag name.
-    * @param {string} tagName - HTML tag name for new DOM element.
-    * @returns {object} New DOM object for given HTML tag.
-    */
-    function gen(tagName) {
-      return document.createElement(tagName);
-    }
+   /**
+   * CHANGE: Describe what your init function does here.
+   */
+   function init() {
 
 
-    function generateCanvas() {
-
-      let canvasElement = gen("canvas");
-      canvasElement.id = "can";
-      canvasElement.width = "900";
-      canvasElement.height = "300";
-      id("canvas-section").append(canvasElement);
-      can = new handwriting.Canvas(id("can"));
-
-      can.setCallBack(function(data) {
-        updateUserInput(data, can);
-      });
-
-      can.setOptions(
-        {
-          language: "en",
-          numOfReturn: 5
-        }
-      );
-
-      ["click", "touchend"].forEach(function(e) {
-        id("can").addEventListener(e,() => {
-          can.recognize();
-        });
-      });
-
-      let clear = gen("button");
-      clear.id = "clear-button";
-      clear.textContent = "clear";
-      id("user-button").append(clear);
-
-      id("clear-button").addEventListener("click", () => {
-        can.erase();
-        clearDisplayText()
-      });
-
-    }
-
-    function clearDisplayText() {
-      let element = qs("#input-result p");
-      element.textContent = "";
-    }
-
-    function updateUserInput(data, can) {
-      updateTextBox(data);
-      //check answer
-      console.log(data);
-    }
-
-    function updateTextBox(data) {
-      let element = qs("#input-result p");
-      let result = data[0].toLowerCase();
-      element.textContent = result;cle
-      let wordAmount = getRandomIntBetween(10, 25);
-
-      for (let i = 0; i < wordAmount; i++) {
-        let currentWord = words[getRandomIndex(words)].toLowerCase();
-
-        quote += currentWord;
-        if (i + 1 != wordAmount) {
-          quote += " ";
-        }
-      }
-
-      return quote;
-    }
-
-    function getRandomQuote() {
-      let quote = "";
-
-      let wordAmount = getRandomIntBetween(10, 25);
-
-      "this is a string"
-      for (let i = 0; i < wordAmount; i++) {
-        let currentWord = words[getRandomIndex(words)].toLowerCase();
-
-        quote += currentWord;
-        if (i + 1 != wordAmount) {
-          quote += " ";
-        }
-      }
-
-      return quote;
-    }
+    // menu
 
 
-    function getRandomIntBetween(min, max) {
-      min = Math.ceil(min);
-      max = Math.floor(max);
-      return Math.floor(Math.random() * (max - min) + min);
-    }
 
-    function getRandomIndex(array) {
-      return Math.floor(Math.random()*array.length);
-    }
+    // queuing screen
 
- })();
+    // game
+
+    // result
+
+     // THIS IS THE CODE THAT WILL BE EXECUTED ONCE THE WEBPAGE LOADS
+     renderNewQuote();
+     generateCanvas();
+
+     console.log(firebase);
+
+   }
+
+   function checkInput() {
+     console.log("changed");
+     let correct = true;
+     const arrayQuoteSpan = quoteDisplayElement.querySelectorAll('span');
+
+     if(valueWord == null) {
+       arrayQuoteSpan[curIndex].classList.remove('correct');
+       arrayQuoteSpan[curIndex].classList.remove('incorrect');
+       correct = false;
+     }
+     else if(arrayQuote[curIndex] === valueWord) {
+       arrayQuoteSpan[curIndex].classList.add('correct');
+       arrayQuoteSpan[curIndex].classList.remove('incorrect');
+       // correct word, moving onto next
+       can.erase();
+       curIndex++;
+     } else {
+       arrayQuoteSpan[curIndex].classList.remove('correct');
+       arrayQuoteSpan[curIndex].classList.add('incorrect');
+       correct = false;
+     }
+
+    //  if(curIndex === arrayQuote.length && correct) renderNewQuote();
+   }
+
+   function renderNewQuote() {
+     curIndex = 0;
+     arrayQuote = [];
+     const quote = getRandomQuote();
+     quoteDisplayElement.innerHTML = '';
+     quote.split(" ").forEach(word => {
+       const wordSpan = gen('span');
+       wordSpan.innerText = word + " ";
+       quoteDisplayElement.appendChild(wordSpan);
+       arrayQuote.push(word);
+     })
+
+     startTimer();
+   }
+
+   let startTime;
+   function startTimer() {
+     timerElement.innerText = 90;
+     startTime = new Date();
+     let timerId = setInterval(() => {
+       timer.innerText = 90 - getTimerTime();
+       if(Number(timer.textContent) === 0) {
+         clearInterval(timerId);
+         renderNewQuote();
+       }
+     }, 1000);
+   }
+
+   function getTimerTime() {
+     return Math.floor((new Date() - startTime) / 1000);
+   }
+
+   /**
+   * Make sure to always add a descriptive comment above
+   * every function detailing what it's purpose is
+   * @param {variabletype} someVariable This is a description of someVariable, including, perhaps, preconditions.
+   * @returns {returntype} A description of what this function is actually returning
+   */
+   function exampleFunction2(someVariable) {
+     /* SOME CODE */
+     return something;
+   }
+
+   /** ------------------------------ Helper Functions  ------------------------------ */
+   /**
+   * Note: You may use these in your code, but remember that your code should not have
+   * unused functions. Remove this comment in your own code.
+   */
+
+   /**
+   * Returns the element that has the ID attribute with the specified value.
+   * @param {string} idName - element ID
+   * @returns {object} DOM object associated with id.
+   */
+   function id(idName) {
+     return document.getElementById(idName);
+   }
+
+   /**
+   * Returns the first element that matches the given CSS selector.
+   * @param {string} selector - CSS query selector.
+   * @returns {object} The first DOM object matching the query.
+   */
+   function qs(selector) {
+     return document.querySelector(selector);
+   }
+
+   /**
+   * Returns the array of elements that match the given CSS selector.
+   * @param {string} selector - CSS query selector
+   * @returns {object[]} array of DOM objects matching the query.
+   */
+   function qsa(selector) {
+     return document.querySelectorAll(selector);
+   }
+
+   /**
+   * Returns a new element with the given tag name.
+   * @param {string} tagName - HTML tag name for new DOM element.
+   * @returns {object} New DOM object for given HTML tag.
+   */
+   function gen(tagName) {
+     return document.createElement(tagName);
+   }
+
+
+   function generateCanvas() {
+     let canvasElement = gen("canvas");
+     canvasElement.id = "can";
+     canvasElement.width = "900";
+     canvasElement.height = "300";
+     id("canvas-section").append(canvasElement);
+     can = new handwriting.Canvas(id("can"));
+
+     can.setCallBack(function(data) {
+       updateUserInput(data, can);
+     });
+
+     can.setOptions(
+       {
+         language: "en",
+         numOfReturn: 5
+       }
+     );
+
+     ["click", "touchend"].forEach(function(e) {
+       id("can").addEventListener(e,() => {
+         can.recognize();
+       });
+     });
+
+     let clear = gen("button");
+     clear.id = "clear-button";
+     clear.textContent = "clear";
+     id("user-button").append(clear);
+
+     id("clear-button").addEventListener("click", () => {
+       can.erase();
+       clearDisplayText()
+     });
+
+   }
+
+   function clearDisplayText() {
+     let element = qs("#input-result p");
+     element.textContent = "";
+   }
+
+   function updateUserInput(data, can) {
+     updateTextBox(data);
+     //check answer
+     console.log(data);
+   }
+
+   function updateTextBox(data) {
+     let element = qs("#input-result p");
+     let result = data[0].toLowerCase();
+     element.textContent = result;
+     valueWord = data[0].toLowerCase();
+     checkInput();
+   }
+
+   function getRandomQuote() {
+     let quote = "";
+
+     let wordAmount = getRandomIntBetween(10, 15);
+
+     "this is a string"
+     for (let i = 0; i < wordAmount; i++) {
+       let currentWord = words[getRandomIndex(words)].toLowerCase();
+
+       quote += currentWord;
+       if (i + 1 != wordAmount) {
+         quote += " ";
+       }
+     }
+
+     return quote;
+   }
+
+   function getRandomIntBetween(min, max) {
+     min = Math.ceil(min);
+     max = Math.floor(max);
+     return Math.floor(Math.random() * (max - min) + min);
+   }
+
+   function getRandomIndex(array) {
+     return Math.floor(Math.random()*array.length);
+   }
+
+})();
